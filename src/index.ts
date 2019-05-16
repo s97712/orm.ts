100b80b9e1e124184c90a2fa1c68ad3356ab055d
s97712 <s97712@qq.com>
2019-05-16 17:36:49 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { StringDataTypeConstructor, NumberDataTypeConstructor, Model, ModelAttributeColumnOptions, DataTypes, Sequelize, ModelAttributeColumnReferencesOptions } from "sequelize";
+import { StringDataTypeConstructor, NumberDataTypeConstructor, Model, ModelAttributeColumnOptions, DataTypes, Sequelize, ModelAttributeColumnReferencesOptions, CreateOptions, BulkCreateOptions } from "sequelize";
 
 class WideUserId {
     constructor(public value?: number) {}
@@ -36,15 +36,29 @@ type ModelColumn<T> = Extract<T, undefined> extends never
 type CreateValues<M> = {
     [P in keyof M]: M[P]
 }
-export class Utils<M> {
+export class Utils<M extends BaseModel> {
     constructor(
         protected model: typeof BasicModal,
         public alias: string,
     ) {}
-    create(user: WideUserId, values: CreateValues<M>) {
-        this.model.create(values)
+
+    find(userId: WideUserId) {
+        this.model.findAll({
+        })
     }
+    create(userId: WideUserId, values: M[], options: BulkCreateOptions)  {
+        return this.model.bulkCreate(values.map(value =>({
+            ...value, userId: userId.value
+        })), options);
+    }
+    createOne(userId: WideUserId, value: M, options: CreateOptions){
+        return this.model.create({
+            ...value, userId: userId.value
+        }, options);
+    }
+    
 } 
+
 
 export class BaseModel {
     private identify(){}
